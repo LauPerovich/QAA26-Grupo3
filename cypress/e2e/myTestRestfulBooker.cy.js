8bb91ce08cc5400ee584305024136ed97fcf0412
Belen1908 <belu_19_08@hotmail.com>
2026-06-23 20:21:56 -0300

    Se agrego test 3.2

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -6,6 +6,53 @@
 // 1. Intentar enviar el formulario sin completar ningún campo
 // 2. Verificar que aparecen los mensajes de error correspondientes
 // 3. Verificar que no se realizó ninguna reserva
+describe('3.2-Validaciones del formulario de reserva', () => {
+
+    it('should create a booking successfully', () => {
+        cy.visit('https://automationintesting.online/')
+        cy.request('GET', 'https://automationintesting.online/')
+            .then((response) => {
+                expect(response.status).to.eq(200)
+            })
+
+        cy.get('.nav-link').filter('[href*="/#rooms"]').click()
+        cy.get('.btn.btn-primary').contains('Book now').click()
+        cy.url().should('include', '/reservation')
+        cy.get('#doReservation').click()
+        cy.get('.btn.btn-primary').contains('Reserve Now').click()
+
+        // Valida error 400, que es un POST por formulario vacio  
+        cy.request({
+            method: 'POST',
+            url: 'https://automationintesting.online/api/booking',
+            failOnStatusCode: false,
+            body: {}
+        }).then((response) => {
+            expect(response.status).to.eq(400)
+        })
+
+        //Valida si alguno de los textos o msj de error son visibles o se muestran en pantalla
+        const expectedErrors = [
+            'size must be between 3 and 18',
+            'size must be between 3 and 30',
+            'must not be empty',
+            'Lastname should not be blank',
+            'size must be between 11 and 21',
+            'Firstname should not be blank',
+        ]
+
+        expectedErrors.forEach((errorMsg) => {
+            cy.get('li').contains(errorMsg).should('exist').should('be.visible')
+        })
+
+    })
+
+})
+
+
+
+
+
 
 // 3.3 Formulario de contacto
 // 1. Completar el formulario de contacto con datos válidos
